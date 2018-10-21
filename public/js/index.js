c8f310e99ee33d57a6481db092fa7988b8f5dae6
kendrajmoore <kendra.j.moore3443@gmail.com>
2018-10-20 20:35:49 -0700

    update

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,9 @@ $(document).ready(()=>{
 
   //Keep track of the current user
   let currentUser;
+  socket.emit('get online users');
+  //Each user should be in the general channel by default.
+socket.emit('user changed channel', "General");
 
   $('#createUserBtn').click((e)=>{
     e.preventDefault();
@@ -55,4 +58,48 @@ $(document).ready(()=>{
     $('#newChannelInput').val("");
   }
  })
+ //Add the new channel to the channels list (Fires for all clients)
+socket.on('new channel', (newChannel) => {
+  $('.channels').append(`
+    <div class="channel">${newChannel}</div>
+  `);
+});
+
+//Make the channel joined the current channel. Then load the messages.
+//This only fires for the client who made the channel.
+socket.on('user changed channel', (data) => {
+  $('.channel-current').addClass('channel');
+  $('.channel-current').removeClass('channel-current');
+  $(`.channel:contains('${data.channel}')`).addClass('channel-current');
+  $('.channel-current').removeClass('channel');
+  $('.message').remove();
+  data.messages.forEach((message) => {
+    $('.messageContainer').append(`
+      <div class="message">
+        <p class="messageUser">${message.sender}: </p>
+        <p class="messageText">${message.message}</p>
+      </div>
+    `);
+  });
+})
+$('#sendChatBtn').click((e) => {
+  e.preventDefault();
+  // Get the client's channel
+  let channel = $('.channel-current').text();
+  let message = $('#chatInput').val();
+  if(message.length > 0){
+    socket.emit('new message', {
+      sender : currentUser,
+      message : message,
+      //Send the channel over to the server
+      channel : channel
+    });
+    $('#chatInput').val("");
+  }
+
+  $(document).on('click', '.channel', (e)=>{
+    let newChannel = e.target.textContent;
+    socket.emit('user changed channel', newChannel);
+  });
+});
 })
--- sockets/chat.js
+++ sockets/chat.js
@@ -42,5 +42,19 @@ socket.on('new channel', (newChannel) => {
       messages : channels[newChannel]
     });
   })
+socket.on('new message', (data) => {
+  //Save the new message to the channel.
+channels[data.channel].push({sender : data.sender, message : data.message});
+  //Emit only to sockets that are in that channel room.
+io.to(data.channel).emit('new message', data);
+});
+//Have the socket join the room of the channel
+socket.on('user changed channel', (newChannel) => {
+  socket.join(newChannel);
+  socket.emit('user changed channel', {
+    channel : newChannel,
+    messages : channels[newChannel]
+  });
+})
 
 }
