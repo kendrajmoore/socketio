aead947a1b36cfedcbcdc9c0517276ce9156778e
kendrajmoore <kendra.j.moore3443@gmail.com>
2018-10-23 08:50:36 -0700

    update view page

--- controllers/routes.js
+++ controllers/routes.js
@@ -4,6 +4,18 @@ const User = require("../models/users.js");
 
 //index page
 router.get("/", (req, res) => {
+    const currentUser = req.user;
+    if (currentUser === null) {
+        return res.redirect("/user/login");
+    }
+    res.render("index.hbs", { currentUser: req.user });
+});
+
+router.get("/admin", (req, res) => {
+    const currentUser = req.user;
+    if (currentUser === null) {
+        return res.redirect("/user/login");
+    }
     res.render("index.hbs", { currentUser: req.user });
 });
 
