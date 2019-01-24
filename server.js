d94fd11eb53041c67ac6d412e1826071cb2c7516
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2019-01-24 12:37:54 -0800

    condensing server

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const methodOverride = require("method-override");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const jwt = require("jsonwebtoken");
 //Socket.io has to use the http server
 const server = require('http').Server(app);
 
@@ -26,8 +25,6 @@ io.on("connection", (socket) => {
 // Port
 const port = process.env.PORT || 3000;
 
-//SET UP MONGOOSE
-const mongoose = require("mongoose");
 
 //middleware
 app.use(methodOverride("_method"));
@@ -41,49 +38,8 @@ app.use(morgan("dev"));
 app.use(express.static("public"));
 
 
-// Mongoose Connection
-const mongoUri =
-    process.env.MONGODB_URI || "mongodb://localhost:27017/socketio";
-mongoose.connect(
-    mongoUri,
-    { useNewUrlParser: true }
-);
-
-//USER AUTH
-var checkAuth = (req, res, next) => {
-    if (
-        typeof req.cookies.nToken === "undefined" ||
-        req.cookies.nToken === null
-    ) {
-        req.user = null;
-    } else {
-        var token = req.cookies.nToken;
-        var decodedToken = jwt.decode(token, { complete: true }) || {};
-        req.user = decodedToken.payload;
-    }
-
-    next();
-};
-app.use(checkAuth);
-
-
-//user routes
-const usersController = require("./controllers/users.js");
-app.use("/user", usersController);
-
-//Express View Engine for Handlebars
-// Set the view engine and file extension
 app.engine("hbs", hbs({ defaultLayout: "main", extname: "hbs" }));
 app.set("view engine", "hbs")
-
-app.get('/', (req, res) => {
-  res.render('homepage.hbs');
-})
-
-//404 page
-app.get("*", (req, res) => {
-    res.render("error.hbs");
-});
 
 server.listen('3000', () => {
   console.log('Server listening on Port 3000');
