e9a00770c2d4efeb03b211ceb8e0799b4ecd4a31
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2019-01-29 07:08:57 -0800

    delete js folder, rewrote server

--- server.js
+++ server.js
@@ -1,52 +1,28 @@
-//App.js
 const express = require('express');
 const app = express();
-const hbs = require("express-handlebars");
-const methodOverride = require("method-override");
-const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
 //Socket.io has to use the http server
 const server = require('http').Server(app);
 
-//Socket.io stuff
-//app.js
+//Socket.io
 const io = require('socket.io')(server);
-//We'll store our online users here
 let onlineUsers = {};
-//Save the channels in this object.
 let channels = {"General" : []}
 io.on("connection", (socket) => {
-  // Make sure to send the users to our chat file
-  require('./sockets/chat.js')(io, socket, onlineUsers, channels);
+    require('./sockets/chat.js')(io, socket, onlineUsers, channels);
 })
 
+//Express View Engine for Handlebars
+const exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
-// Port
-const port = process.env.PORT || 3000;
-
-
-//middleware
-app.use(methodOverride("_method"));
-// Use Body Parser
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(morgan("dev"));
-
-// static files middleware
-app.use(express.static("public"));
-
-
-app.engine("hbs", hbs({ defaultLayout: "main", extname: "hbs" }));
-app.set("view engine", "hbs")
+//Establish your public folder
+app.use('/public', express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('index.hbs');
-  })
-
-server.listen('3000', () => {
-  console.log('Server listening on Port 3000');
+    res.render('index.handlebars');
 })
 
-module.exports = app;
+server.listen('3000', () => {
+    console.log('Server listening on Port 3000');
+})
